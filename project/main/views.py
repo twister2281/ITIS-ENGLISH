95797b9e097bb29ff75b4df1fe27847061659d1e
Maksim Fomin <f.maksim.2005@gmail.com>
2025-05-27 11:08:48 +0300

    Система взаимодействия с карточками

--- project/main/views.py
+++ project/main/views.py
@@ -1,14 +1,59 @@
-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+from main.utils.utils import MasterCard
+from main.models import Profile
+
+def get_user_profile(request) -> Profile:
+    if not request.user.is_authenticated:
+        return HttpResponse("You must be authorized to access this page", status=401)
+    
+    return request.user.profile
+
+def dont_remember_card(request):
+    profile = get_user_profile(request)
+    
+    return MasterCard.dont_remember_card(profile.id, request.POST.get('card_id'))
+
+def remember_card(request):
+    profile = get_user_profile(request)
+    
+    return MasterCard.remember_card(profile.id, request.POST.get('card_id'))
+
+def remove_card(request):
+    profile = get_user_profile(request)
+    
+    return MasterCard.remove_card(profile.id, request.POST.get('card_id'))
+
+def get_cards(request):
+    profile = get_user_profile(request)
+    
+    data = MasterCard.get_cards(profile.id, request.POST.get('number'))
+    return JsonResponse(data, safe=False)
+
+def get_new_card_from_dict(request):
+    profile = get_user_profile(request)
+    
+    return MasterCard.get_new_card_from_dict(profile.id)
+
+def add_user_card(request):
+    profile = get_user_profile(request)
+    
+    data = MasterCard.add_user_card(profile.id, request.POST.get('word'), request.POST.get('translation'))
+    return JsonResponse(data)
+
+def edit_card(request):
+    profile = get_user_profile(request)
+    
+    data = MasterCard.edit_card(profile.id, request.POST.get('word'), request.POST.get('translation'), request.POST.get('card_id'))
+    return JsonResponse(data)
+
+
+def learning(request):
+    return render (request, 'main/learning.html')
 
 def index(request):
-    data = {
-        'title': 'Welcome to My Website',
-        'values': ['value1', 'value2', 'value3'],
-    }
     return render (request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("This is the about page.")
+    pass
